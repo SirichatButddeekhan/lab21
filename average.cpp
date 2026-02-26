d5e6878c93ebcc12888bea6f7cc69c732cc09a05
sirichat <sirichatcnx60@gmail.com>
2026-02-27 00:05:29 +0700

    finish

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
 
+
+int main(int argc,char * argv[]){
+    double sum = 0.0;
+    double avg;
+    int count;
+    count = argc-1;
+    if (argc == 1) {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+    for(int i= 1;i < argc;i++){
+        sum += stod(argv[i]);
+    }
+    avg = sum/count;
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << count << " numbers = " << avg << endl;
+    cout << "---------------------------------" << endl;
+
+}
